f9f7bddf6b0f0ba74fb833b7fa2e17ef872f5d29
Gunn-Treepaech <treepaech@kkumail.com>
2023-09-30 22:29:28 +0700

    add miniproject ai

---
+++ AI/script-AI.js
@@ -0,0 +1,77 @@
+// script.js
+let result = [];
+let bestPredictionIndex = 0;
+let bestClassPrediction;
+const URL = "https://teachablemachine.withgoogle.com/models/V_2rnQ1r_/";
+let model, webcam, labelContainer, maxPredictions;
+
+window.onload = async function () {
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  const flip = true;
+  webcam = new tmImage.Webcam(200, 200, flip);
+  await webcam.setup();
+  await webcam.play();
+  window.requestAnimationFrame(loop);
+
+  document.getElementById("webcam-container").appendChild(webcam.canvas);
+  labelContainer = document.getElementById("label-container");
+  for (let i = 0; i < maxPredictions; i++) {
+    labelContainer.appendChild(document.createElement("div"));
+  }
+};
+
+async function loop() {
+  webcam.update();
+  window.requestAnimationFrame(loop);
+}
+
+// สร้างฟังก์ชันเพื่อลบผลลัพธ์เก่าออก
+function clearResults() {
+  result = [];
+  for (let i = 0; i < 4; i++) {
+    labelContainer.childNodes[i].innerHTML = ""; // ลบข้อความที่แสดงผลออก
+  }
+}
+
+async function captureImage() {
+  const prediction = await model.predict(webcam.canvas);
+  clearResults();
+  // หาค่าความน่าจะเป็นสูงสุดและเก็บดัชนีของมัน
+  let maxProbability = 0;
+  for (let i = 0; i < maxPredictions; i++) {
+    if (prediction[i].probability > maxProbability) {
+      maxProbability = prediction[i].probability;
+      bestPredictionIndex = i;
+    }
+  }
+
+  // แสดงผลลัพธ์ที่มีความน่าจะเป็นสูงสุดเท่านั้น
+  bestClassPrediction =
+    prediction[bestPredictionIndex].className +
+    ": " +
+    prediction[bestPredictionIndex].probability.toFixed(2);
+  labelContainer.childNodes[bestPredictionIndex].innerHTML =
+    bestClassPrediction;
+
+  // เก็บผลลัพธ์ทั้งหมดไว้ในตัวแปร global result
+  result = prediction;
+  console.log(bestClassPrediction);
+  console.log(result);
+
+  // เช็คเงื่อนไขและแสดง GIF ตามเงื่อนไขที่คุณต้องการ
+  if (bestClassPrediction.includes("general waste")) {
+    document.getElementById("gif-display").src =
+      "/AI/images/cherry-blossoms-6383_256.gif";
+  } else if (bestClassPrediction.includes("Hazardous waste")) {
+    document.getElementById("gif-display").src =
+      "/AI/images/halloween-7291_256.gif";
+  } else if (bestClassPrediction.includes("recycled waste")) {
+    document.getElementById("gif-display").src = "/AI/images/sun-6751_256.gif";
+  } else if (bestClassPrediction.includes("solid waste")) {
+    document.getElementById("gif-display").src = "/AI/images/rose-6870_256.gif";
+  }
+}
